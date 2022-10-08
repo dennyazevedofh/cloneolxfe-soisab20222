e209f5ac2378b74c0993a3548c670b55f3a68eb9
DennyAzevedo <denny_af@yahoo.com.br>
2022-10-08 10:29:21 -0300

    Pagina Anuncio terminada

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -4,12 +4,21 @@ React,
 	useState,
 	useEffect
 } from 'react';
-import { PageArea, Fake } from './styled';
+import {
+	PageArea,
+	Fake,
+	OthersArea,
+	BreadChumb
+} from './styled';
 import { PageContainer } from '../../components/MainComponents';
-import { useParams } from 'react-router-dom';
+import {
+	useParams,
+	Link
+} from 'react-router-dom';
 import { Slide } from 'react-slideshow-image';
 import "react-slideshow-image/dist/styles.css";
 import useApi from '../../helpers/OlxAPI';
+import AdItem from '../../components/partials/AdItem';
 
 const Page = () => {
 	const api = useApi();
@@ -50,6 +59,23 @@ const Page = () => {
 
 	return (
 		<PageContainer>
+			{adInfo.category &&
+				<BreadChumb>
+					Você está aqui:
+					<Link to="/">
+						Home
+					</Link>
+					/
+					<Link to={`/ads?state=${adInfo.stateName}`}>
+						{adInfo.stateName}
+					</Link>
+					/
+					<Link to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}>
+						{adInfo.category.name}
+					</Link>
+					/ {adInfo.title}
+				</BreadChumb>
+			}
 			<PageArea>
 				<div className="leftSide">
 					<div className="box">
@@ -89,12 +115,58 @@ const Page = () => {
 				<div className="rightSide">
 					<div className="box box--padding">
 						{loading && <Fake height={20} />}
+						{adInfo.priceNegotiable &&
+							"Preço Negociável"
+						}
+						{!adInfo.priceNegotiable && adInfo.price &&
+							<div className="price">
+								Preço:
+								<span>
+									R$ {adInfo.price}
+								</span>
+							</div>
+						}
 					</div>
-					<div className="box box--padding">
-						{loading && <Fake height={50} />}
-					</div>
+					{loading && <Fake height={50} />}
+					{adInfo.userInfo &&
+						<>
+							<a
+								href={`mailto:${adInfo.userInfo.email}`}
+								target="_blank"
+								rel="noreferrer"
+								className="contactSellerLink"
+							>
+								Fale com o vendedor
+							</a>
+							<div className="createBy box box--padding">
+								<strong>
+									{adInfo.user.name}
+								</strong>
+								<small>
+									E-mail: {adInfo.userInfo.email}
+								</small>
+								<small>
+									Estado: {adInfo.userInfo.stateName}
+								</small>
+							</div>
+						</>
+					}
 				</div>
 			</PageArea>
+			<OthersArea>
+				{adInfo.others &&
+					<>
+						<h2>
+							Outras ofertas do vendedor
+						</h2>
+						<div className="list">
+							{adInfo.others.map((i, k) => 
+								<AdItem key={k} data={i} />
+							)}
+						</div>
+					</>
+				}
+			</OthersArea>
 		</PageContainer>
 	)
 }
